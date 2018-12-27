39418f21a934c3189ccdd72eaaf83f30e3c9d4aa
Massu <imomushi.pipi@gmail.com>
2018-12-27 22:53:20 +0900

    結果画面実装に伴う変更

--- server.py
+++ server.py
@@ -19,19 +19,26 @@ def index():
     led.clear()
     return render_template('index.html')
 
+
 @app.route('/omikuji')
 def omikuji():
     global led
     return render_template('omikuji.html')
 
-@app.route('/send',methods=['GET','POST'])
+
+@app.route('/send', methods=['GET', 'POST'])
 def display():
     global led
     data = request.json
-    app.logger.debug(data)
+    # app.logger.debug(data)
     led.display(data)
-    return ''
+    if led.res_flg:  # 結果が出力済みの場合
+        led.res_flg = False  # 初期化
+        return render_template('result.html')
+    else:
+        return ''
+
 
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     app.run(host='0.0.0.0', port=8000, threaded=True)
